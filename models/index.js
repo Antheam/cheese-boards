e32c40e4fa03f4961618597660a805430542fe6c
antheam <anthea.merton@gmail.com>
2022-11-04 14:10:30 +0000

    created index.js file

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const User = require('./user.model')
+const Cheese = require('./cheese.model')
+const Board = require('./board.model')
+
+
+//one to one relationship
+User.hasMany(Board)
+Board.belongsTo(User)
+
+//many to many relationship
+Board.belongsToMany(Cheese, {through: 'Board_Cheese'})
+Cheese.belongsToMany(Board, {through: 'Board_Cheese'})
+
+
+
+
+
+
+module.exports = {User, Cheese, Board}
